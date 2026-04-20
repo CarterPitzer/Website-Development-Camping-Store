b2361f21ada7c10262c246b1e38445ee49868793
CarterPitzer <cpitzer7@ivytech.edu>
2026-04-20 16:57:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// MOBILE MENU TOGGLE
+
 function toggleMenu() {
     document.getElementById("nav-links").classList.toggle("show");
 }
 
-// CONTACT FORM
+
 function submitContact(event) {
     event.preventDefault();
     document.getElementById("contact-success").innerText =
         "Your message has been sent!";
 }
 
-// LOGIN FORM
+
 function submitLogin(event) {
     event.preventDefault();
     document.getElementById("login-success").innerText =
         "Login successful!";
 }
 
-// TRACK ORDER FORM
+
 function trackOrder(event) {
     event.preventDefault();
     let orderNum = document.getElementById("order-number").value;
